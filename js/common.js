1b970b6772235c01025966baea20729d50573141
Alexliew829 <alexliew2026@gmail.com>
2026-08-11 02:26:50 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2.90";
+const APP_VERSION = "5.2";
 
 function formatMoney(value, prefix = "") {
   const number = Number(value) || 0;
--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "lover-legend-import-cost-v2.90-stable";
+const CACHE = "lover-legend-import-cost-V5.2-fixed-cost-lock";
 const CORE = [
   "./",
-  "./index.html?v=2.90",
-  "./css/style.css?v=2.90",
-  "./js/common.js?v=2.90",
-  "./js/sync.js?v=2.90",
-  "./js/app.js?v=2.90",
-  "./manifest.json?v=2.90",
+  "./index.html?v=4.9",
+  "./css/style.css?v=4.9",
+  "./js/common.js?v=4.9",
+  "./js/sync.js?v=4.9",
+  "./js/app.js?v=4.9",
+  "./manifest.json?v=4.9",
   "./assets/images/logo-green.jpg",
   "./assets/images/logo-red.jpg",
   "./assets/icons/favicon.ico",
@@ -41,7 +41,7 @@ self.addEventListener("fetch", event => {
       fetch(event.request)
         .then(response => {
           const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put("./index.html?v=2.90", copy));
+          caches.open(CACHE).then(cache => cache.put("./index.html?v=4.9", copy));
           return response;
         })
         .catch(() => caches.match("./index.html"))
