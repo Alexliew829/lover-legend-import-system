cf73c80e790705acc5ebd006dd93d35e2f36630a
Alexliew829 <alexliew2026@gmail.com>
2026-09-22 17:20:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "lover-legend-import-cost-v33.6-stable";
+const CACHE = "lover-legend-import-cost-v32.8-stable";
 const CORE = [
   "./",
-  "./index.html?v=33.6",
-  "./css/style.css?v=33.6",
-  "./js/common.js?v=33.6",
-  "./js/sync.js?v=33.6",
-  "./js/app.js?v=33.6",
-  "./manifest.json?v=33.6",
+  "./index.html?v=32.8",
+  "./css/style.css?v=32.8",
+  "./js/common.js?v=32.8",
+  "./js/sync.js?v=32.8",
+  "./js/app.js?v=32.8",
+  "./manifest.json?v=32.8",
   "./assets/images/logo-green.jpg",
   "./assets/images/logo-red.jpg",
   "./assets/icons/favicon.ico",
@@ -41,7 +41,7 @@ self.addEventListener("fetch", event => {
       fetch(event.request)
         .then(response => {
           const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put("./index.html?v=33.6", copy));
+          caches.open(CACHE).then(cache => cache.put("./index.html?v=32.8", copy));
           return response;
         })
         .catch(() => caches.match("./index.html"))
